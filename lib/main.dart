3131145a3c3cb147650f1de06bebf93626072630
Donkor- <donkor@yeah.net>
2021-04-22 00:19:11 +0800

    使用http进行网络请求。同时适配Android/ios关于域名http请求的限制

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,91 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_trip/navigator/tab_navigator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _showResult = "";
+
+  Future<CommonModel> fetchPost() async {
+    var url = Uri.parse(
+        'http://www.devio.org/io/flutter_app/json/test_common_model.json');
+    final response = await http.get(url);
+    final result = json.decode(response.body);
+    return CommonModel.fromJson(result);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Http'),
+        ),
+        body: Column(
+          children: [
+            InkWell(
+              onTap: () {
+                fetchPost().then((value) => setState(() {
+                  _showResult = "请求结果：\n icon : ${value.icon}"
+                          "\n title : ${value.title}"
+                          "\n url : ${value.url}"
+                          "\n statusBarColor : ${value.statusBarColor}"
+                          "\n hideAppBar : ${value.hideAppBar}";
+                    }));
+              },
+              child: Text(
+                '点我',
+                style: TextStyle(fontSize: 30),
+              ),
+            ),
+            Text(_showResult),
+          ],
+        ),
       ),
-      home: TabNavigator(),
     );
   }
 }
 
+class CommonModel {
+  final String icon;
+  final String title;
+  final String url;
+  final String statusBarColor;
+  final bool hideAppBar;
 
+  CommonModel(
+      {this.icon, this.title, this.url, this.statusBarColor, this.hideAppBar});
 
+  factory CommonModel.fromJson(Map<String, dynamic> json) {
+    return CommonModel(
+      icon: json['icon'],
+      title: json['title'],
+      url: json['url'],
+      statusBarColor: json['statusBarColor'],
+      hideAppBar: json['hideAppBar'],
+    );
+  }
+}
+
+// class MyApp extends StatelessWidget {
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: TabNavigator(),
+//     );
+//   }
+// }
