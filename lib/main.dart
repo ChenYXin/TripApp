62b9de7b911bf7504fc2b0b5c4db7ba96fb9bb03
Donkor <donkor@yeah.net>
2021-04-22 20:22:33 +0800

    FutureBuilder实践

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,9 @@ class _MyAppState extends State<MyApp> {
     var url = Uri.parse(
         'http://www.devio.org/io/flutter_app/json/test_common_model.json');
     final response = await http.get(url);
-    final result = json.decode(response.body);
+    //fix 中文乱码
+    Utf8Decoder utf8decoder = Utf8Decoder();
+    final result = json.decode(utf8decoder.convert(response.bodyBytes));
     return CommonModel.fromJson(result);
   }
 
@@ -30,26 +32,40 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('Http'),
         ),
-        body: Column(
-          children: [
-            InkWell(
-              onTap: () {
-                fetchPost().then((value) => setState(() {
-                  _showResult = "请求结果：\n icon : ${value.icon}"
-                          "\n title : ${value.title}"
-                          "\n url : ${value.url}"
-                          "\n statusBarColor : ${value.statusBarColor}"
-                          "\n hideAppBar : ${value.hideAppBar}";
-                    }));
-              },
-              child: Text(
-                '点我',
-                style: TextStyle(fontSize: 30),
-              ),
-            ),
-            Text(_showResult),
-          ],
-        ),
+        body: FutureBuilder<CommonModel>(
+            future: fetchPost(),
+            builder:
+                (BuildContext context, AsyncSnapshot<CommonModel> snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  return Text('Input a URL to start');
+                case ConnectionState.waiting:
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                case ConnectionState.active:
+                  return Text('');
+                case ConnectionState.done:
+                  if (snapshot.hasError) {
+                    return Text(
+                      '${snapshot.error}',
+                      style: TextStyle(color: Colors.red),
+                    );
+                  } else {
+                    return Column(
+                      children: [
+                        Text('icon : ${snapshot.data.icon}'),
+                        Text('title : ${snapshot.data.title}'),
+                        Text('url : ${snapshot.data.url}'),
+                        Text(
+                            'statusBarColor : ${snapshot.data.statusBarColor}'),
+                        Text('hideAppBar : ${snapshot.data.hideAppBar}'),
+                      ],
+                    );
+                  }
+              }
+              return Text('');
+            }),
       ),
     );
   }
@@ -75,6 +91,70 @@ class CommonModel {
     );
   }
 }
+
+// class _MyAppState extends State<MyApp> {
+//   String _showResult = "";
+//
+//   Future<CommonModel> fetchPost() async {
+//     var url = Uri.parse(
+//         'http://www.devio.org/io/flutter_app/json/test_common_model.json');
+//     final response = await http.get(url);
+//     final result = json.decode(response.body);
+//     return CommonModel.fromJson(result);
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Http'),
+//         ),
+//         body: Column(
+//           children: [
+//             InkWell(
+//               onTap: () {
+//                 fetchPost().then((value) => setState(() {
+//                   _showResult = "请求结果：\n icon : ${value.icon}"
+//                       "\n title : ${value.title}"
+//                       "\n url : ${value.url}"
+//                       "\n statusBarColor : ${value.statusBarColor}"
+//                       "\n hideAppBar : ${value.hideAppBar}";
+//                 }));
+//               },
+//               child: Text(
+//                 '点我',
+//                 style: TextStyle(fontSize: 30),
+//               ),
+//             ),
+//             Text(_showResult),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+// class CommonModel {
+//   final String icon;
+//   final String title;
+//   final String url;
+//   final String statusBarColor;
+//   final bool hideAppBar;
+//
+//   CommonModel(
+//       {this.icon, this.title, this.url, this.statusBarColor, this.hideAppBar});
+//
+//   factory CommonModel.fromJson(Map<String, dynamic> json) {
+//     return CommonModel(
+//       icon: json['icon'],
+//       title: json['title'],
+//       url: json['url'],
+//       statusBarColor: json['statusBarColor'],
+//       hideAppBar: json['hideAppBar'],
+//     );
+//   }
+// }
 
 // class MyApp extends StatelessWidget {
 //   // This widget is the root of your application.
